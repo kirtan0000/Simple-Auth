83ba86a58d6caa49e7c1dd79c38eb3ed0efcd94a
kirtan <kirtanmagan@gmail.com>
2022-01-30 21:40:25 -0800

    Connection Release At Open + Easier Connection Errors

--- util/connection.ts
+++ util/connection.ts
@@ -1,17 +1,33 @@
-import mysql from "mysql";
-const enviromentVars = require("dotenv").config().parsed;
-import util from "util";
-var con;
+import mysql from 'mysql'
+const enviromentVars = require('dotenv').config().parsed
+import util from 'util'
+var con
 
 // Create a new connection with the MySQl database with the given credentials
 const create = () => {
   con = mysql.createPool({
     user: enviromentVars.USER.toString(),
     password: enviromentVars.PASSWORD.toString(),
-    database: enviromentVars.DATABASE.toString(),
-  });
-  con.getConnection(() => {});
-  con.query = util.promisify(con.query); // Prevent async/await errors
-};
+    database: enviromentVars.DATABASE.toString()
+  })
+  con.getConnection((err, connection) => {
+    if (err) {
+      if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+        console.error('Database connection was closed.')
+      }
+      if (err.code === 'ER_CON_COUNT_ERROR') {
+        console.error('Database has too many connections.')
+      }
+      if (err.code === 'ECONNREFUSED') {
+        console.error('Database connection was refused.')
+      }
+    }
 
-export { create, con };
+    if (connection) connection.release()
+
+    return
+  })
+  con.query = util.promisify(con.query) // Prevent async/await errors
+}
+
+export { create, con }
